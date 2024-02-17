7ffe5a25a01a1716add25772687308b07c5cba77
kvartiil <138615442+kvartiil@users.noreply.github.com>
2024-02-17 23:07:05 +0200

    Initial commit

---
+++ src/app/subpageVeraCard/subpageVeraCard.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { SubpageVeraCardPageRoutingModule } from './subpageVeraCard-routing.module';
+
+import { SubpageVeraCardPage } from './subpageVeraCard.page';
+
+//import { VeraRobotComponent } from '../matrixVeraRobot/matrixVeraRobot.component';
+//import { VVeraRobotComponent } from '../matrixVeraRobot/matrix-vrobot-msg/matrix-vrobot-msg.component';
+
+import { VeraFlashComponent } from '../matrixVeraFlash/matrixVeraFlash.component';
+
+//import { TamarComponent } from '../matrixTamar/matrixTamar.component';
+//import { WestPromenadeComponent } from '../matrixWestPromenade/matrixWestPromenade.component';
+//import { PlayConsultantComponent } from '../matrixPlayConsultant/matrixPlayConsultant.component';
+//import { PlayConsultant2Component } from '../matrixPlayConsultant2/matrixPlayConsultant2.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    SubpageVeraCardPageRoutingModule
+  ],
+  declarations: [SubpageVeraCardPage, VeraFlashComponent]
+})
+export class SubpagePageModule {}
